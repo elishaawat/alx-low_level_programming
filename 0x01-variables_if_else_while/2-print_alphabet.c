f4332f74ae8f6a87026cb4b34a93e5a727231e97
elishaawat <elishaawatii@gmail.com>
2022-04-09 06:17:36 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * Main -Entry point
+ *
+ * Return: Always 0 (Success /correct)
+ */
+int main (void)
+{
+	char alphabet;
+	
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+
+		putchar(alphabet);
+	}
+
+return (0);
+
+}
+	
