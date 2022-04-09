79dfba0d5464f392895ffaee15945f502cc0735a
elishaawat <elishaawatii@gmail.com>
2022-04-09 01:29:20 -0700

    ee

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main -Entry point
- * My hello,print task
- * return: return 1 after program execution
+ * main - Entry point
+ * My hello, print task
+ * Return: return 1 after program execution
  */
 int main(void)
 {
