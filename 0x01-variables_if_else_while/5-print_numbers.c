686133c2f4eaaedabe11ae332716fb8001aadc0d
elishaawat <elishaawatii@gmail.com>
2022-04-09 07:23:16 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return :Always 0 (Sucess/corect)
+ */
+
+int main(void)
+
+{
+	int i;
+	
+	for (i =0 ; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
