2ab74f27a0399df2a865c725f7598ad4632865b5
elishaawat <elishaawatii@gmail.com>
2022-04-11 01:08:26 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * main - Entry point
  * My hello, print task
  * Return: return 1 after program execution
- */
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
---
+++ 0x00-hello_world/alx-low_level_programming/README.md
@@ -0,0 +1,1 @@
+ this is a alx-low_level_programming project file
