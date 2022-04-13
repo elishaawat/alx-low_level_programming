c37e70740d3d69334b5d1377b12dbb5e0abab176
elishaawat <elishaawatii@gmail.com>
2022-04-13 11:37:53 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,33 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+#include "main.h"
+/**
+ *main - Entry point
+ *
+ *Return: nothing, return void
+ */
+int main(void)
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
+{
 
-#endif
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+
+
+	return (0);
+
+}
