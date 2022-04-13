6ffb04b2d96d55e7dd34a1edb98d1ea72adbeeac
elishaawat <elishaawatii@gmail.com>
2022-04-13 15:37:07 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,15 +1,16 @@
 #include"main.h"
 /**
- * Main -Entry point
- * Return :void
+ *jack_bauer - Prints every minute of the day 00:00 to 23:59
+ * @void - no argument
  */
 void jack_bauer(void)
 {
-	int i, j;
+	int i;
+       int j;
 
-	for (i = 0; i < 24; i++)
+	for (i = 0; i < 23; i++)
 	{
-		for (j = 0; j < 60; j++)
+		for (j = 0; j < 59; j++)
 		{
 			_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,44 +1,36 @@
 #include "main.h"
 /**
- * times_table(void)
- * Return void
+ * times_table -function that prints the 9 times table starting with 0
+ *  @avoid:no arguments
  */
 void times_table(void)
 {
-	int a = 0;
+	int i,j,k;
 
-	int b;
-
-	int rep;
-
-	while (a <= 9)
+	for  ( i = 0; i <= 9; i ++)
 	{
-		b = 0;
-
-		while (b <=9)
+		for (j = 0; j <= 9; j++)
 		{
-			rep = a * b;
-			if (b == 0)
+			k = i *j;
+			if (j == 0)
 			{
-				_putchar('0' + rep );
+				_putchar(k + '0');
 			}
-			else if(rep < 10)
+			else if (k <= 9)
 			{
-				_putchar(' ')
-	_putchar('0'0 + rep);
-			}
-			else
-			{
-	_putchar('0' + rep / 10);
-_putchar('0' +rep  10);
-			}
-			if (b < 9) 
 				_putchar(',');
-			_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k <= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(k / 10 +'0');
+				_putchar(k % 10 + '0');
+			}
 		}
-		b++;
+_putchar('\n');		
 	}
-	putchar('\n');
-	a++;
-}
 }
