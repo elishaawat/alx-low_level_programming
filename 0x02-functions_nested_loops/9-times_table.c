6649ad608de4e7f132b7995bb16f6f03d2205dab
elishaawat <elishaawatii@gmail.com>
2022-04-13 15:50:02 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,32 +5,43 @@
  */
 void times_table(void)
 {
-	int i,j,k;
+	int a;
+	int b;
+	int c;
 
-	for  ( i = 0; i <= 9; i ++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (b = 0; b <= 9; b++)
 		{
-			k = i *j;
-			if (j == 0)
+			c = a * b;
+			if ((c / 10) == 0)
 			{
-				_putchar(k + '0');
+				if (b == 0)
+				{
+					_putchar ('0');
+				}
+				if (b != 0)
+				{
+					_putchar (' ');
+					_putchar ((c % 10) + '0');
+				}
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar (' ');
+				}
 			}
-			else if (k <= 9)
+			else
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			}
-			else if (k <= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(k / 10 +'0');
-				_putchar(k % 10 + '0');
+				_putchar ((c / 10) + '0');
+				_putchar ((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar (' ');
+				}
 			}
 		}
-_putchar('\n');		
+		_putchar ('\n');
 	}
 }
