e357529dd8be3e97415e1d6735d6248b14c6b200
elishaawat <elishaawatii@gmail.com>
2022-04-12 19:23:26 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,16 @@
 void print_alphabet_x10(void)
 {
 	char c;
+
 	int i = 0;
+
 	while (i < 10)
 	{
 		c = 'a';
 
 		while (c <= 'z')
 		{
-		_putchar(c);											c++;
+			_putchar(c);											c++;
 
 		}
 
