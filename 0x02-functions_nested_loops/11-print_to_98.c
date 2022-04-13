adc39ef18af409e5177929a249204d7c6608e937
elishaawat <elishaawatii@gmail.com>
2022-04-12 19:48:54 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,35 +6,18 @@
  * Return:0 or 1
  */
 void print_to_98(int n)
-
 {
-
-
-
 	while (n < 98)
-
 	{
-
-			printf("%i, ", n);
-
-				n++;
-
+		printf("%i, ", n);
+		n++;
 	}
-
 	while (n > 98)
-
 	{
-
-			printf("%i, ", n);
-
-				n--;
-
+		printf("%i, ", n);
+		n--;
 	}
-
-
-
-	printf("98");
+	intf("98");
 
 	putchar('\n');
-
 }
